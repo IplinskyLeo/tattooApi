99a7f893429201f3bf2387d6516b441107e69ce4
IplinskyLeo <leoipy16@gmail.com>
2022-08-16 16:59:06 -0300

    db finished

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Running on port: ${port}`);
+})
---
+++ src/database/create-and-populate.js
@@ -0,0 +1,45 @@
+import sqlite3 from "sqlite3";
+
+const db = new sqlite3.Database("./src/database/database.db");
+
+const createTable = () => {
+  `CREATE TABLE IF NOT EXISTS 'MATERIAL' (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        fornecedor TEXT,
+        produto TEXT,
+        quantidade TEXT,
+        valor INTEGER
+        )`;
+};
+
+const populateTable = () => {
+  `INSERT INTO MATERIAL (fornecedor, produto, quantidade, valor) VALUES (
+        "Trixxy",
+        "caneta rotativa T3100",
+        "5",
+        "350"
+        )`,
+    `INSERT INTO MATERIAL (fornecedor, produto, quantidade, valor) VALUES (
+        "Master Agulhas",
+        "agulha",
+        "50",
+        "20"
+        )`,
+    `INSERT INTO MATERIAL (fornecedor, produto, quantidade, valor) VALUES (
+        "Pro Clean",
+        "kit higienizador",
+        "10",
+        "30"
+        )`,
+    `INSERT INTO MATERIAL (fornecedor, produto, quantidade, valor) VALUES (
+        "Max Tintas",
+        "tintas variadas",
+        "100",
+        "14"
+        )`;
+};
+
+db.serialize(() => {
+  createTable();
+  populateTable();
+});
