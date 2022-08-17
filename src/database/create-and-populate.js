5dc37e39ed7d9038cbdb1e24810b791865f1c89f
IplinskyLeo <leoipy16@gmail.com>
2022-08-17 11:33:22 -0300

    create and populated fixed names

--- src/database/create-and-populate.js
+++ src/database/create-and-populate.js
@@ -1,41 +1,59 @@
 import sqlite3 from "sqlite3";
-const db = new sqlite3.Database("./src/database/database.db");
+const db = new sqlite3.Database("./database.db");
 
 const createTable = () => {
-  `CREATE TABLE IF NOT EXISTS FORNECEDOR (
+  db.run(
+    `CREATE TABLE IF NOT EXISTS MATERIAL (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         fornecedor TEXT,
         produto TEXT,
         quantidade TEXT,
         valor INTEGER
-        )`;
+        )`,
+    (err) => {
+      if (err) {
+        console.log(err + "Error to create table");
+      } else {
+        console.log("sucessfully created table");
+      }
+    }
+  );
 };
 
 const populateTable = () => {
-  `INSERT INTO FORNECEDOR (fornecedor, produto, quantidade, valor) VALUES (
+  db.run(
+    `INSERT INTO MATERIAL (fornecedor, produto, quantidade, valor) VALUES (
         "Trixxy",
         "caneta rotativa T3100",
         "5",
         "350"
         )`,
-    `INSERT INTO FORNECEDOR (fornecedor, produto, quantidade, valor) VALUES (
+    `INSERT INTO MATERIAL (fornecedor, produto, quantidade, valor) VALUES (
         "Master Agulhas",
         "agulha",
         "50",
         "20"
         )`,
-    `INSERT INTO FORNECEDOR (fornecedor, produto, quantidade, valor) VALUES (
+    `INSERT INTO MATERIAL (fornecedor, produto, quantidade, valor) VALUES (
         "Pro Clean",
         "kit higienizador",
         "10",
         "30"
         )`,
-    `INSERT INTO FORNECEDOR (fornecedor, produto, quantidade, valor) VALUES (
+    `INSERT INTO MATERIAL (fornecedor, produto, quantidade, valor) VALUES (
         "Max Tintas",
         "tintas variadas",
         "100",
         "14"
-        )`;
+        )`,
+    (err) => {
+      if (err) {
+        console.log(err + "Error to populate table");
+      } else {
+        console.log("sucessfully populated table");
+      }
+    }
+  );
 };
 
 db.serialize(() => {
